b143c925e9bd91ec5f893212e0a0b87235dbc182
solitary-cloud <1946306647@qq.com>
2022-10-08 17:12:16 +0800

    地图接入

---
+++ pages/red-envelope/red-envelope.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="map-container">
+    <map
+      :longitude="113.293719"
+      :latitude="22.885983"
+      :scale="20"
+      enable-poi
+      enable-3D
+      style="width: 100vw; height: 100vh"
+    >
+    </map>
+  </view>
+</template>
+
+<script>
+export default {
+  onLoad() {
+    this.$nextTick(() => {
+      uni.getLocation({
+        type: "wgs84",
+        success: function (res) {
+          console.log("当前位置的经度：" + res.longitude);
+          console.log("当前位置的纬度：" + res.latitude);
+        },
+        fail: () => {
+        },
+        complete: function () {
+        },
+      });
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
