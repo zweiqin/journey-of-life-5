8a224e39411403958898570f335b83283d149a37
solitary-cloud <1946306647@qq.com>
2022-12-07 14:50:06 +0800

    绘制达人页面

---
+++ marketing-treasure-house/daren-square/apply-daren.vue
@@ -0,0 +1,16 @@
+<template>
+  <view class="apply-daren"></view>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.apply-daren {
+  width: 100%;
+  height: auto;
+  min-height: 100vh;
+  background-color: #f6f6f6;
+}
+</style>
