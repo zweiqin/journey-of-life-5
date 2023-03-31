a2366799fa9735c688162b0db562bf577de6b763
jinxingliu2022 <Asdf1234!>
2023-03-31 18:34:55 +0800

    团油跳转

--- pages/index/convenient-services/tuanyouh5.vue
+++ pages/index/convenient-services/tuanyouh5.vue
@@ -1,47 +1,33 @@
 <template>
 	<view>
 		<view>
-			<!-- url为要跳转外链的地址-->
 			<!-- #ifdef APP -->
 			<web-view :src="url"></web-view>
 			<!-- #endif -->
 			<!-- #ifdef H5 -->
-			<view class="ddd">
-				<iframe :src="url" height="100%" width="100%"></iframe>
+			<view class="fullcrseen">
+				<iframe :src="url" height="100%" width="100%" frameborder="0"></iframe>
 			</view>
 			<!-- #endif -->
 		</view>
 	</view>
 </template>
-
 <script>
-// import { items, coupons ,tests ,tests1} from './data'
-// import { RuanRequest,getUserId } from "../../../utils"
-// import { payOrderGoodsApi } from "../../../api/goods"
 export default {
-		data() {
-			return {
-				url: ''
-			}
-		},
-		onLoad(val) {
-			console.log(val);
-			this.url=val.url + "&token=" + val.token + "&timestamp=" + val.timestamp
-			+ "&sign=" + val.sign;
-		},
-		methods: {
-			isNotEmpty(obj) {
-				if (typeof obj == undefined || obj == null || obj == "" || obj == "undefined" || obj.length == 0) {
-					return false;
-				} else {
-					return true;
-				}
-			},
+	data() {
+		return {
+			url: ''
 		}
+	},
+	onLoad(val) {
+		this.url=val.url + "&token=" + val.token + "&timestamp=" + val.timestamp + "&sign=" + val.sign;
+	},
+	methods: {
 	}
+}
 </script>
 <style>
-	.ddd {
+	.fullcrseen {
 		height: 100vh;
 	}
 </style>
