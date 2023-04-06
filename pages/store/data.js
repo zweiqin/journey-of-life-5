6b9cbe642b4471d489c27b86bd3ae85ce193b09e
zweiqin <zweiqin88@163.com>
2023-04-06 19:11:36 +0800

    jf本地生活二级

--- pages/store/data.js
+++ pages/store/data.js
@@ -1,42 +1,57 @@
 export const menusData = [
-  {	id:1,
-    name: '美食饮品',
-    icon: require('../../static/brand/menus/meishi.png'),
-    url: '/pages/store/fine-food/fine-food'
-  },
-  {	id:2,
-    name: '休闲娱乐',
-    icon: require('../../static/brand/menus/xiuxian.png'),
-		url:'/pages/store/fine-food/food-nearby/food-nearby'
-  },
-  { id:3,
-    name: '美甲美睫',
-    icon: require('../../static/brand/menus/meijia.png'),
-		url: '/pages/store/fine-food/food-nearby/food-nearby'
-  },
-  {
-    name: '运动健身',
-    icon: require('../../static/brand/menus/yundong.png'),
-		url: '/pages/store/fine-food/food-nearby/food-nearby'
-  },
-  {
-    name: '美容美发',
-    icon: require('../../static/brand/menus/meirong.png'),
-		url: '/pages/store/fine-food/food-nearby/food-nearby'
-  },
-  {
-    name: '周边旅游',
-    icon: require('../../static/brand/menus/zhoubian.png'),
-		url: '/pages/store/fine-food/food-nearby/food-nearby'
-  },
-  {
-    name: '亲子乐园',
-    icon: require('../../static/brand/menus/qinzi.png'),
-		url: '/pages/store/fine-food/food-nearby/food-nearby'
-  },
-  {
-    name: '酒店民俗',
-    icon: require('../../static/brand/menus/jiudian.png'),
-		url: '/pages/store/fine-food/food-nearby/food-nearby'
-  },
+	{
+		id: 1,
+		name: '美食饮品',
+		icon: require('../../static/brand/menus/meishi.png'),
+		url: '/pages/store/fine-food/fine-food'
+	},
+	{
+		id: 2,
+		name: '休闲娱乐',
+		icon: require('../../static/brand/menus/xiuxian.png'),
+		url: '/pages/store/fine-food/food-nearby/food-nearby',
+		title: '休闲娱乐',
+	},
+	{
+		id: 3,
+		name: '美甲美睫',
+		icon: require('../../static/brand/menus/meijia.png'),
+		url: '/pages/store/fine-food/food-nearby/food-nearby',
+		title: '美甲美睫'
+	},
+	{
+		id: 4,
+		name: '运动健身',
+		icon: require('../../static/brand/menus/yundong.png'),
+		url: '/pages/store/fine-food/food-nearby/food-nearby',
+		title: '运动健身'
+	},
+	{
+		id: 5,
+		name: '美容美发',
+		icon: require('../../static/brand/menus/meirong.png'),
+		url: '/pages/store/fine-food/food-nearby/food-nearby',
+		title: '美容美发'
+	},
+	{
+		id: 6,
+		name: '周边旅游',
+		icon: require('../../static/brand/menus/zhoubian.png'),
+		url: '/pages/store/fine-food/food-nearby/food-nearby',
+		title: '周边旅游'
+	},
+	{
+		id: 7,
+		name: '亲子乐园',
+		icon: require('../../static/brand/menus/qinzi.png'),
+		url: '/pages/store/fine-food/food-nearby/food-nearby',
+		title: '亲子乐园',
+	},
+	{
+		id: 8,
+		name: '酒店民俗',
+		icon: require('../../static/brand/menus/jiudian.png'),
+		url: '/pages/store/fine-food/food-nearby/food-nearby',
+		title: '酒店民俗',
+	},
 ]
--- pages/store/fine-food/food-nearby/food-nearby.vue
+++ pages/store/fine-food/food-nearby/food-nearby.vue
@@ -1,191 +1,211 @@
 <template>
-  <view class="food-nearby-page-container">
-    <view class="fixed-container">
-      <view class="search-container">
-        <view class="search-header">
-          <BeeBack>
-            <view class="header-title">
-              <BeeIcon :size="24" color="#000" name="arrowleft"></BeeIcon>
-              <h1>附近美食</h1>
-            </view>
-          </BeeBack>
-          <SearchBar background="#fff"></SearchBar>
-          <view class="control">
-            <view class="item">
-              <BeeIcon
-                :size="24"
-                :src="
-                  require('../../../../static/brand/find-food/food-nearby/location.png')
-                "
-              ></BeeIcon>
-              <text>位置</text>
-            </view>
-          </view>
-        </view>
-      </view>
+	<view class="food-nearby-page-container">
+		<view class="fixed-container">
+			<view class="search-container">
+				<view class="search-header">
+					<BeeBack>
+						<view class="header-title">
+							<BeeIcon :size="24" color="#000" name="arrowleft"></BeeIcon>
+							<h1>{{ this.title }}</h1>
+						</view>
+					</BeeBack>
+					<SearchBar background="#fff"></SearchBar>
+					<view class="control">
+						<view class="item">
+							<BeeIcon :size="24" :src="
+								require('../../../../static/brand/find-food/food-nearby/location.png')
+							"></BeeIcon>
+							<text>位置</text>
+						</view>
+					</view>
+				</view>
+			</view>
 
-      <view class="menus-wrapper">
-        <view
+			<view class="menus-wrapper">
+				<!-- <view
           @click="handleChooseMenu(item)"
           class="item"
           :class="{ active: currentMenu === item.id }"
           v-for="item in menus"
           :key="item.id"
         >
-          <!-- <BeeIcon :size="40" :src="item.icon"></BeeIcon> -->
+          <BeeIcon :size="40" :src="item.icon"></BeeIcon>
           <text>{{ item.name }}</text>
-        </view>
-      </view>
+        </view> -->
 
-      <FilterPane></FilterPane>
-    </view>
+				<view @click="handleChooseMenu(item)" class="item" :class="{ active: currentList === item.text }"
+					v-for="item in list" :key="item.text">
+					<text>{{ item.text }}</text>
+				</view>
+			</view>
 
-    <view class="brand-list">
-      <BeeBrandPane
-        v-for="item in $data._list"
-        :key="item.id"
-        :brand-info="item"
-      ></BeeBrandPane>
-      <LoadMore :status="$data._status"></LoadMore>
-    </view>
-  </view>
+			<FilterPane></FilterPane>
+		</view>
+
+		<view class="brand-list">
+			<BeeBrandPane v-for="item in $data._list" :key="item.id" :brand-info="item"></BeeBrandPane>
+			<LoadMore :status="$data._status"></LoadMore>
+		</view>
+	</view>
 </template>
 
 <script>
 import loadData from '../../../../mixin/loadData'
 import { getSroreListApi } from '../../../../api/store'
 import FilterPane from './cpns/FilterPane.vue'
-import { menus } from './data'
+import { menus, navs } from './data'
 export default {
-  components: {
-    FilterPane,
-  },
-  mixins: [
-    loadData({
-      api: getSroreListApi,
-      mapKey: {
-        list: 'brandList',
-        totalPages: 'totalPages',
-        size: 'size',
-      },
-      dataFn(data) {
-        const ignoreBrandList = ['佛山市顺德区修江缘美食餐饮店', '测试门店呀']
-        return data.filter(item => {
-          return !ignoreBrandList.includes(item.name)
-        })
-      },
-    }),
-  ],
-  data() {
-    return {
-      currentMenu: 1001002,
-      menus: Object.freeze(menus),
-    }
-  },
+	components: {
+		FilterPane,
+	},
+	mixins: [
+		loadData({
+			api: getSroreListApi,
+			mapKey: {
+				list: 'brandList',
+				totalPages: 'totalPages',
+				size: 'size',
+			},
+			dataFn(data) {
+				const ignoreBrandList = ['佛山市顺德区修江缘美食餐饮店', '测试门店呀']
+				return data.filter(item => {
+					return !ignoreBrandList.includes(item.name)
+				})
+			},
+		}),
+	],
+	data() {
+		return {
+			currentMenu: 1001002,
+			currentList:'好店推荐',
+			menus: Object.freeze(menus),
+			navs,
+			list:[],
+		}
+	},
 
-  onLoad() {
-    this._loadData()
-  },
+	onLoad(options) {
+		this._loadData()
+		this.getNavsList()
+		this.title = options.name
+		this.id = options.id
 
-  methods: {
-    handleChooseMenu(item) {
-      this.currentMenu = item.id
-    },
-  },
+		
+	},
+
+	methods: {
+		handleChooseMenu(item) {
+			this.currentMenu = item.id
+			this.currentList = item.text
+		},
+		//导航栏
+		async getNavsList() {
+			const data = navs
+			console.log('数据', data);
+			const nid = data[0].id
+			console.log('类id', nid);
+			this.nid = nid
+			const list = data[0].list
+			console.log('list',list);
+			this.list = list
+			
+			
+		}
+	},
 }
 </script>
 
 <style lang="scss" scoped>
 .food-nearby-page-container {
-  width: 100vw;
-  min-height: 100vh;
-  background: linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, #f6f6f6 0%);
+	width: 100vw;
+	min-height: 100vh;
+	background: linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, #f6f6f6 0%);
 
-  .fixed-container {
-    .search-header {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      padding: 44upx 22upx 0;
-      box-sizing: border-box;
+	.fixed-container {
+		.search-header {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			padding: 44upx 22upx 0;
+			box-sizing: border-box;
 
-      .header-title {
-        display: flex;
-        align-items: center;
-      }
+			.header-title {
+				display: flex;
+				align-items: center;
+			}
 
-      h1 {
-        font-size: 28upx;
-        color: #000;
-        font-weight: normal;
-        margin-right: 10upx;
-      }
+			h1 {
+				font-size: 28upx;
+				color: #000;
+				font-weight: normal;
+				margin-right: 10upx;
+			}
 
-      /deep/ .search-bar-container {
-        flex: 1;
-        margin-right: 15upx;
-      }
+			/deep/ .search-bar-container {
+				flex: 1;
+				margin-right: 15upx;
+			}
 
-      .control {
-        display: flex;
-        align-items: center;
+			.control {
+				display: flex;
+				align-items: center;
 
-        .item {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-direction: column;
-          color: #000;
-          font-size: 24upx;
-          margin-left: 8upx;
+				.item {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					flex-direction: column;
+					color: #000;
+					font-size: 24upx;
+					margin-left: 8upx;
 
-          &:nth-child(1) {
-            margin-left: 0;
-          }
-        }
-      }
-    }
+					&:nth-child(1) {
+						margin-left: 0;
+					}
+				}
+			}
+		}
 
-    .menus-wrapper {
-      white-space: nowrap;
-      display: flex;
-      align-items: center;
-      overflow: scroll;
-      padding: 20upx 20upx 0 20upx;
+		.menus-wrapper {
+			white-space: nowrap;
+			display: flex;
+			align-items: center;
+			overflow: scroll;
+			padding: 20upx 20upx 0 20upx;
 
-      .item {
+			.item {
 				height: 48upx;
-        position: relative;
-        // width: 120upx;
-        // height: 160upx;
-        // display: flex;
-        // align-items: center;
-        // justify-content: center;
-        // flex-direction: column;
-        font-size: 24upx;
-        // padding: 16upx 12upx;
-        box-sizing: border-box;
-        margin-right: 48upx;
-        border-radius: 10upx;
-        flex-shrink: 0;
-        // background-color: #fff;
+				position: relative;
+				// width: 120upx;
+				// height: 160upx;
+				// display: flex;
+				// align-items: center;
+				// justify-content: center;
+				// flex-direction: column;
+				font-size: 24upx;
+				// padding: 16upx 12upx;
+				box-sizing: border-box;
+				margin-right: 48upx;
+				border-radius: 10upx;
+				flex-shrink: 0;
+				// background-color: #fff;
 
-        // &.active {
-        //   border: 1upx solid #fa5151;
+				// &.active {
+				//   border: 1upx solid #fa5151;
 
-        //   &::after {
-        //     content: '';
-        //     bottom: 0;
-        //     right: 0;
-        //     width: 28upx;
-        //     height: 22upx;
-        //     position: absolute;
-        //     display: block;
-        //     background: url('../../../../static/brand/find-food/food-nearby/menus/active.png')
-        //       no-repeat;
-        //     background-size: cover;
-        //   }
-        // }
+				//   &::after {
+				//     content: '';
+				//     bottom: 0;
+				//     right: 0;
+				//     width: 28upx;
+				//     height: 22upx;
+				//     position: absolute;
+				//     display: block;
+				//     background: url('../../../../static/brand/find-food/food-nearby/menus/active.png')
+				//       no-repeat;
+				//     background-size: cover;
+				//   }
+				// }
 
 				&::after {
 					content: '';
@@ -210,14 +230,14 @@ export default {
 				}
 
 
-      }
-    }
-  }
+			}
+		}
+	}
 
-  .brand-list{
-    padding: 0 20upx;
-    box-sizing: border-box;
-    
-  }
+	.brand-list {
+		padding: 0 20upx;
+		box-sizing: border-box;
+
+	}
 }
 </style>
